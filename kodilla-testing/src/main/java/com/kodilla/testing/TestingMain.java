805d71845455b42e3fe889fc506bd1d17c83f29f
unknown <pk@bluzo.pl>
2018-10-09 17:00:12 +0200

    Modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,26 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
     public static void main(String[] args){
-            System.out.println("Moduł 6. wprowadzenie do testowania oprogramowania");
+
+        Calculator count = new Calculator(10,12);
+
+        int resultAdd = count.add();
+        int resultSubstract = count.substract();
+
+        if (resultAdd == 22){
+            System.out.println("Test result correct -> " + count.getA() + " + " + count.getB() + " = " + resultAdd);
+        } else {
+            System.out.println("Test result error -> "  + count.getA() + " + " + count.getB() + " = " + resultAdd + "\n");
+        }
+
+        if (resultSubstract == -2){
+            System.out.println("Test result correct -> " + count.getA() + " - " + count.getB() + " = " + resultSubstract);
+        } else {
+            System.out.println("Test result: error (-> " + count.getA() + " - " + count.getB() + " = " + resultSubstract);
+        }
     }
 }
