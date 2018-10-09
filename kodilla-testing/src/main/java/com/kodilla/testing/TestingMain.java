d55cb64c10633b0fc633ab35c50c1546be28392f
unknown <pk@bluzo.pl>
2018-10-09 17:36:17 +0200

    Modul 6.2

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -6,21 +6,21 @@ import com.kodilla.testing.user.SimpleUser;
 public class TestingMain {
     public static void main(String[] args){
 
-        Calculator count = new Calculator(10,12);
+        Calculator count = new Calculator();
 
-        int resultAdd = count.add();
-        int resultSubstract = count.substract();
+        int resultAdd = count.add(12,10);
+        int resultSubstract = count.substract(11, 5);
 
         if (resultAdd == 22){
-            System.out.println("Test result correct -> " + count.getA() + " + " + count.getB() + " = " + resultAdd);
+            System.out.println("Test result correct");
         } else {
-            System.out.println("Test result error -> "  + count.getA() + " + " + count.getB() + " = " + resultAdd + "\n");
+            System.out.println("Test result error" + "\n");
         }
 
-        if (resultSubstract == -2){
-            System.out.println("Test result correct -> " + count.getA() + " - " + count.getB() + " = " + resultSubstract);
+        if (resultSubstract == 6){
+            System.out.println("Test result correct");
         } else {
-            System.out.println("Test result: error (-> " + count.getA() + " - " + count.getB() + " = " + resultSubstract);
+            System.out.println("Test result error" + "\n");
         }
     }
 }
--- kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -4,20 +4,14 @@ public class Calculator {
         private int a;
         private int b;
 
-        public Calculator(int a, int b) {
-            this.a = a;
-            this.b = b;
-        }
-        public int add() {
+        public int add(int a, int b) {
             return a + b;
         }
-
-        public int substract() {
+        public int substract(int a, int b) {
             return  a - b;
         }
 
         public int getA() { return a; }
-
         public int getB() { return b; }
 
 }
