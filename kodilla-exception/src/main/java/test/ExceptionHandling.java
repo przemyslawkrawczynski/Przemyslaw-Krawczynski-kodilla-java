556c8ff49aac39c6c1382239e5dabdbbd84d4319
unknown <pk@bluzo.pl>
2018-10-28 20:01:51 +0100

    Moduł 8.32

--- kodilla-exception/src/main/java/test/ExceptionHandling.java
+++ kodilla-exception/src/main/java/test/ExceptionHandling.java
@@ -1,4 +1,18 @@
 package test;
 
 public class ExceptionHandling {
+    public static void main(String[] args) {
+        SecondChallenge sc = new SecondChallenge();
+
+        try {
+
+            String newEx = sc.probablyIWillThrowException(2, 1.5);
+            System.out.println(newEx);
+
+        } catch (Exception e) {
+
+            System.out.println("We have new Exception-->" + e);
+
+        }
+    }
 }
