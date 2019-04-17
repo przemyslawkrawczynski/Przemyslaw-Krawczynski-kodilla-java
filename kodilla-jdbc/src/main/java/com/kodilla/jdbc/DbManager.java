2764089424e5caa16bc952bdd27b178a06851009
pkrawczynski <pk@bluzo.pl>
2019-04-17 16:41:40 +0200

    Java - call UpdateBestsellers

--- kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
+++ kodilla-jdbc/src/main/java/com/kodilla/jdbc/DbManager.java
@@ -11,11 +11,10 @@ public class DbManager {
 
     private DbManager() throws SQLException {
         Properties connectionProps = new Properties();
-        connectionProps.put("user", "kodilla_user");
-        connectionProps.put("password", "kodilla_password");
+        connectionProps.put("user", "hibernate2");
+        connectionProps.put("password", "QWEasd123!");
         conn = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/kodilla_course?serverTimezone=Europe/Warsaw" +
-                        "&useSSL=False",
+                "jdbc:mysql://localhost:3306/hibernate2?serverTimezone=Europe/Warsaw&useSSL=False&allowPublicKeyRetrieval=true",
                 connectionProps);
     }
 
